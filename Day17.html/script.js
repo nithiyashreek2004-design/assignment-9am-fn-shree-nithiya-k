43c744ef34b05c1060a5d8f209909cae470778a4
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2025-11-26 22:28:12 +0530

    update

---
+++ Day17.html/script.js
@@ -0,0 +1,20 @@
+let name = "Shree Nithya K";
+console.log("Hello, " + name + "! Welcome to Day 17.");
+
+var age =21;
+console.log("I am " + age + " years old.");
+
+
+function wish() {
+    console.log("Good Morning");
+}
+
+wish();
+
+
+
+
+// JavaScript is a programming language used to make web pages interactive.
+// It can update content, handle events, validate forms, create animations,
+// and control the behavior of websites in the browser.
+
