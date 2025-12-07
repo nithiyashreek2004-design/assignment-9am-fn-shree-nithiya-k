2e3ff7ce5636e445dd774d101c61af6fa4be9091
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2025-12-07 14:11:34 +0530

    update

---
+++ Day21.html/practical.js
@@ -0,0 +1,37 @@
+
+// <script>
+// let num = prompt("Enter a number to print its multiplication table:");
+// num = Number(num);
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${num} x ${i} = ${num * i}`);
+// }
+// // </script>
+
+
+// let numbers = [10, 20, 30, 40, 50];
+// let searchValue = 60;
+
+// let found = false;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] === searchValue) {
+//     console.log(`Value ${searchValue} found at index ${i}`);
+//     found = true;
+//     break;  // stop loop when found
+//   }
+// }
+
+// if (!found) {
+//   console.log(`Value ${searchValue} not found`);
+// }
+
+let y = 4;
+do
+{
+  console.log("the password is correct");
+}
+while 
+  (y < 3);
+    console.log("try again");
+
