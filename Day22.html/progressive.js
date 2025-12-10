86e58e08ee63c8219197344525506f9fc80efc48
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2025-12-10 17:19:02 +0530

    update

--- Day22.html/progressive.js
+++ Day22.html/progressive.js
@@ -18,22 +18,41 @@ for (let n of nums) {
 
 //<button> onvlick="sayHello()"> click me </button>
 function sayHello(){
-    console.log(sayHello);
+  console.log(sayHello);
 } 
   sayHello="hello from the button!"; //--------html button function
 
 
-function add(a, b) { return a + b; }
-function sub(a, b) { return a - b; }
-function mul(a, b) { return a * b; }
-function div(a, b) { return a / b; }
+// function add(a, b) { return a + b; }
+// function sub(a, b) { return a - b; }
+// function mul(a, b) { return a * b; }
+// function div(a, b) { return a / b; }
 
-console.log(add(5, 3));
-console.log(sub(10, 4));
-console.log(mul(7, 6));
-console.log(div(20, 5)); //--------modular calculator
+// console.log(add(5, 3));
+// console.log(sub(10, 4));
+// console.log(mul(7, 6));
+// console.log(div(20, 5)); //--------modular calculator
 
-const greet = (name) => `Hello, ${name}!`;
 
-console.log(greet("Shree")); //---------Arrow function
 
+const greet = (name) => `Hello, ${name}!Glad to see you here`;
+
+console.log(greet("Buddy")); //---------Arrow function
+
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const div = (a, b) => b !== 0 ? (a/b) : "cannot divide by zero";
+console.log(add(10, 5));
+console.log(sub(10, 5));
+console.log(mul(10, 5));
+console.log(div(10, 5));
+
+const printEven = (arr) => {
+  for (let n of arr) {
+    if (n % 2 === 0) console.log("Even:", n);
+  }
+};
+
+printEven([1, 2, 3, 4, 5, 6]);
