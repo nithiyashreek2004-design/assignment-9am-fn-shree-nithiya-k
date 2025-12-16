16c3f9ec7bae23a4d33fe5ecb3fa22e12a74e16e
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2025-12-16 11:30:03 +0530

    update

--- Day23.html/progress.js
+++ Day23.html/progress.js
@@ -1,4 +1,7 @@
-const { createBrotliCompress } = require("node:zlib");
+//1
+
+const { Console } = require("console");
+const { title } = require("process");
 
 class product {
     constructor(id,name,price){
@@ -7,6 +10,8 @@ class product {
         this.price = price;
     }
 }
+
+
 class user {
     constructor(name,email){
         this.name = name;
@@ -16,18 +21,82 @@ class user {
         return`welcome, ${this.name}!`;
     }
 }
-class cart{
-    constructor(product){
-        this.name =[];
+
+
+class Cart{
+    constructor(){
+        this.item =[];
     }
     addproduct(product){
         this.item.push(product);
     }
+
 }
 const p1 = new product(1,"laptop",49000);
 console.log(p1);
+
 const u1 = new user("shree","shree@example.com");
 console.log(u1.greet());
+
 const cart = new Cart();
-cart.addProduct(new Product(1, "Mobile", 15000));
-console.log("cart details:"cart);
+cart.addproduct(new product(1, "Mobile", 15000));
+cart.addproduct(new product(2, "Neckband", 1500));
+cart.addproduct(new product(3, "Ironbox", 2000));
+console.log("cart detail:",cart);
+
+
+//2
+class Button {
+    constructor(label,onClick){
+        this.label = label;
+        this.onClick = onClick;
+    }
+    press(){
+        return this.onClick();
+    }
+}
+const loginBtn =new Button("Login",()=>"login button pressed");
+console.log(loginBtn.press());
+
+class Modal{
+constructor(){
+this.visible = false;
+}
+open(){
+this.visible = true;
+return "Modal opened";
+}
+close(){
+this.visible=false;
+return "Modal closed";
+}
+}
+const modal = new Modal();
+console.log(modal.open());
+console.log(modal.close());
+
+class FormHandler{
+submit(data){
+return`Form submitted with:${JSON.stringify(data)}`;
+}
+}
+const form = new FormHandler();
+console.log(form.submit({name: "shree",age:22}));
+
+
+const productData ={
+    id:1,
+    name:"keyboard",
+    price:1200,
+};
+console.log(productData);
+
+const props ={
+    title:"welcome",
+    message:"This is a React-like example"
+};
+const{title,message}=props;
+console.log(title);
+console.log(message);
+
+
