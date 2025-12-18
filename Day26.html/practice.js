9ebf692259c545fcf3ca17d21f908f4c98099ad7
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2025-12-18 19:10:50 +0530

    update

--- Day26.html/practice.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("changeTextBtn").onclick = function () {
-  document.getElementById("heading").textContent = "Text Changed!";
-};
-
-document.getElementById("colorBtn").onclick = function () {
-  document.querySelector("p").style.color = "navy blue";
-};
