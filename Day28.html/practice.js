4eadaae93a2d934ff8221223910a190ded54e283
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2025-12-21 18:57:57 +0530

    update

---
+++ Day28.html/practice.js
@@ -0,0 +1,26 @@
+// <!-- Identify syntax errors in given code
+// Fix a runtime error
+// Find logical mistake in condition
+// Read and explain console error
+// Use console.log to debug -->
+
+//fix a runtime error
+let user;
+console.log(user.name);
+
+
+//logical mistake in code
+let marks = 35;
+if (marks * 40) {
+  console.log("Pass");
+} else {
+  console.log("Fail");
+}
+
+
+//console debug  
+let a = 10;
+let b = 0;
+console.log("b value:", b);
+console.log(a / b);
+
